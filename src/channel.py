a818db5f5178a8cba2b575cd67f6681ad387e1f1
Evgeniya <osipova.dr17@yandex.ru>
2023-11-07 23:04:12 +1100

    make class channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+import os
+import json
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    API_KEY = os.getenv('YOUTUBE_API_KEY')
+    youtube = build("youtube", "v3", developerKey=API_KEY)
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        request_channel_data = self.youtube.channels().list(part='snippet,statistics', id=self.channel_id)
+        channel_data: object = request_channel_data.execute()
+        
+        print(json.dump(channel_data, indent=2, ensure_ascii=False))
